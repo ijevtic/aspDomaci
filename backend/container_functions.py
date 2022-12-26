23ccd42c1e062f85a45119931f849a20b47529d0
mihailot01 <mihailo.t.001@gmail.com>
2022-12-26 02:59:12 +0100

    pm2 bug with container fix

--- backend/container_functions.py
+++ backend/container_functions.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 def run_container(cont_name, image_name, id):
     load_dotenv()
     root = os.getenv('ROOT_PATH')
-    cmd = ['docker', 'run', '--name', cont_name, '-it', '-v', f'{root}/docker_output:/opt/app/data/output', 
+    cmd = ['docker', 'run', '--name', cont_name, '-v', f'{root}/docker_output:/opt/app/data/output', 
     '-v', f'{root}/task_data/task_data{id}:/opt/app/data/task_data',
     "--network", "none", image_name, '/opt/app/data/run_all.sh', str(id)]
-    subprocess.run( cmd, stdout=subprocess.PIPE )
-
+    # result = subprocess.Popen( cmd, stdout=subprocess.PIPE )
+    result = subprocess.call(cmd)
+    print("RUN RES ",result)
 
 def stop_container(name):
     cmd = ['docker', 'stop', name]
