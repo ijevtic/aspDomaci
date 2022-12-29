e139a9a86cedd732d08d738dd16efb9dc2679e55
mihailot01 <mihailo.t.001@gmail.com>
2022-12-29 04:33:26 +0100

    add subtask3

--- backend/docker_data/run_script.sh
+++ backend/docker_data/run_script.sh
@@ -3,8 +3,25 @@
 # $2 task_id 
 if test -f ./code$1; then
     mkfifo fifo
-    for i in {0..1}
-    do
-        ./checker_$2 < fifo $1 $i | ./code$1 > fifo
-    done
+    if [ $2 = 'subtask1' ] 
+    then
+        for i in {0..2}
+        do
+            ./checker_$2 < fifo $1 $i | ./code$1 > fifo
+        done
+    fi
+    if [ $2 = 'subtask2' ] 
+    then
+        for i in {0..1}
+        do
+            ./checker_$2 < fifo $1 $i | ./code$1 > fifo
+        done
+    fi
+    if [ $2 = 'subtask3' ] 
+    then
+        for i in {0..1}
+        do
+            ./checker_$2 < fifo $1 $i | ./code$1 > fifo
+        done
+    fi
 fi
