6500f4c144358ff2e2da1cbcc7d634ae7999b29b
mihailot01 <mihailo.t.001@gmail.com>
2022-12-30 02:23:29 +0100

    minor bug fix for fantastic performance improvement

--- backend/docker_data/run_script.sh
+++ backend/docker_data/run_script.sh
@@ -7,21 +7,21 @@ if test -f ./code$1; then
     then
         for i in {0..2}
         do
-            timeout 1s ./checker_$2 < fifo $1 $i | ./code$1 > fifo
+            timeout 1s ./checker_$2 < fifo $1 $i | timeout 1s ./code$1 > fifo
         done
     fi
     if [ $2 = 'subtask2' ] 
     then
         for i in {0..1}
         do
-            timeot 1s ./checker_$2 < fifo $1 $i | ./code$1 > fifo
+            timeout 1s ./checker_$2 < fifo $1 $i | timeout 1s ./code$1 > fifo
         done
     fi
     if [ $2 = 'subtask3' ] 
     then
         for i in {0..1}
         do
-            timeout 1s ./checker_$2 < fifo $1 $i | ./code$1 > fifo
+            timeout 1s ./checker_$2 < fifo $1 $i | timeout 1s ./code$1 > fifo
         done
     fi
 fi
