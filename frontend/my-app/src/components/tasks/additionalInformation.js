2d437b3e1a002c4fd174930933ffd5f97e90f4e9
mihailot01 <mihailo.t.001@gmail.com>
2023-01-02 21:38:45 +0100

    status code info

--- frontend/my-app/src/components/tasks/additionalInformation.js
+++ frontend/my-app/src/components/tasks/additionalInformation.js
@@ -23,7 +23,7 @@ function AdditionalInfo() {
       <ul styles={{ "list-style-type": "circle" }}>
         <LiStyled>Rok za predaju domaćeg zadatka je <b> petak 13.1.2023. 23:59.</b></LiStyled>
         <LiStyled>Za prvi i treći zadatak je omogućeno automatsko proveravanje re&scaron;enja zadataka, dok je za drugi zadatak potrebno da re&scaron;enje po&scaron;aljete na <u>mtrajkovic@raf.rs</u> i <u>ijevtic@raf.rs</u>.</LiStyled>
-        <LiStyled>Prvi i treći zadatak potrebno je iskucati u programskom jeziku C++, drugi zadatak je moguće raditi u jeziku po izboru.</LiStyled>
+        <LiStyled>Prvi i treći zadatak potrebno je iskucati u programskom jeziku <b>C++</b>, drugi zadatak je moguće raditi u jeziku po izboru.</LiStyled>
         <LiStyled>Kada po&scaron;aljete zadatak na automatsku proveru, ishod ćete dobiti na stranici zadatka, sa desne strane (tu se nalaze sva poslata re&scaron;enja).</LiStyled>
         <LiStyled>Ne postoji limit u broju pokušaja po zadatku, ali je između dva slanja koda na testiranje potrebno da prođe bar 60s.</LiStyled>
         <LiStyled>Od svih poslatih re&scaron;enja gleda se ono koje je donelo najvi&scaron;e poena.</LiStyled>
@@ -35,6 +35,10 @@ function AdditionalInfo() {
         <LiStyled>Veličina izvornog koda u prvom i trećem zadatku je ograničena na 64KB.</LiStyled>
         <LiStyled>U prvom zadatku su tekst drugog i trećeg podzadatka isti - jedina razlika je maksimalan broj 
           bacanja jaja.</LiStyled>
+        <LiStyled>Status kod poslatog rešenja može biti OK, WA ili TLE. 
+          <br />OK označava da je rešenje tačno, WA da sprat nije pogođen ili je upotrebljen veći broj poteza od očekivanog, TLE znači da se kod izvršava duže nego što je dozvoljeno.
+          <br />Ograničenje za izvršavanje koda je 2s za prva tri podzadatka, a 3s za četvrti.</LiStyled>
+
       </ul>
     </DivStyled>
   )
