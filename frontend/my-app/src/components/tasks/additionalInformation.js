ec6ec5904da5942ef39995459e45f831c6a4149d
mihailot01 <mihailo.t.001@gmail.com>
2023-01-02 19:51:19 +0100

    minor text changes

--- frontend/my-app/src/components/tasks/additionalInformation.js
+++ frontend/my-app/src/components/tasks/additionalInformation.js
@@ -21,14 +21,14 @@ function AdditionalInfo() {
       {/* <p>ide gas</p> */}
       <TitleStyled>Dodatne Informacije</TitleStyled>
       <ul styles={{ "list-style-type": "circle" }}>
-        <LiStyled>Rok za predaju domaćeg zadatka je <b>13.1.2023. 23:59.</b></LiStyled>
+        <LiStyled>Rok za predaju domaćeg zadatka je <b> petak 13.1.2023. 23:59.</b></LiStyled>
         <LiStyled>Za prvi i treći zadatak je omogućeno automatsko proveravanje re&scaron;enja zadataka, dok je za drugi zadatak potrebno da re&scaron;enje po&scaron;aljete na <u>mtrajkovic@raf.rs</u> i <u>ijevtic@raf.rs</u>.</LiStyled>
-        <LiStyled>Kada po&scaron;aljete zadatak na automatsku proveru, ishod ćete dobiti na stranici zadatka, sa desne strane(tu se nalaze sva poslata re&scaron;enja).</LiStyled>
-        <LiStyled>Ne postoji limit u broju pokušaja po zadatku, ali je timeout za slanje 60s.</LiStyled>
+        <LiStyled>Kada po&scaron;aljete zadatak na automatsku proveru, ishod ćete dobiti na stranici zadatka, sa desne strane (tu se nalaze sva poslata re&scaron;enja).</LiStyled>
+        <LiStyled>Ne postoji limit u broju pokušaja po zadatku, ali je između dva slanja koda na testiranje potrebno da prođe bar 60s.</LiStyled>
         <LiStyled>Od svih poslatih re&scaron;enja gleda se ono koje je donelo najvi&scaron;e poena.</LiStyled>
-        <LiStyled>Ukupna vrednost svih zadataka je 10 poena(4-3-3).</LiStyled>
-        <LiStyled>Token posle logovanje ističe za ~1h. Kada istekne token, moguće je da će vas sistem preusmeriti
-          da se ponovo prijavite, ili će vas automatski prijaviti ponovo.</LiStyled>
+        <LiStyled>Ukupna vrednost svih zadataka je 10 poena (4-3-3).</LiStyled>
+        {/* <LiStyled>Token posle logovanje ističe za ~1h. Kada istekne token, moguće je da će vas sistem preusmeriti
+          da se ponovo prijavite, ili će vas automatski prijaviti ponovo.</LiStyled> */}
         <LiStyled><strong>Prepisivanje i deljenje kodova je zabranjeno.</strong>&nbsp; Svi studenti koji budu prepisivali ili delili svoj kod drugima će dobiti 0 poena na domaćem zadatku.</LiStyled>
         <LiStyled>Sva eventualna pitanja možete slati na <u>mtrajkovic@raf.rs</u> i&nbsp;<u>ijevtic@raf.rs</u>.</LiStyled>
         <LiStyled>Veličina izvornog koda u prvom i trećem zadatku je ograničena na 64KB.</LiStyled>
--- frontend/my-app/src/components/tasks/task2.js
+++ frontend/my-app/src/components/tasks/task2.js
@@ -4,9 +4,9 @@ const body = (
     <>
         <TitleStyled>Grafički prikaz rotacija AVL stabla</TitleStyled>
         <PStyled>Potrebno je napraviti program za komandnu liniju u jeziku po izboru u 
-            kom treba da omogućite prikaz AVL stabla nakon dodavanja/brisanja čvorova. 
-            Izgled AVL stabla potrebno je prikazati nakon svake rotacije(LL,LR,RL,RR). Potrebno je da se čvorovi i veze
-            jasno vide u konzoli nakon svakog ispisa. Način prikaza se ostavlja studentima.
+            kom treba da omogućite prikaz AVL stabla nakon dodavanja/brisanja čvorova. <br /> 
+            Izgled AVL stabla potrebno je prikazati nakon svake rotacije (LL,LR,RL,RR).  <br /> Potrebno je da se čvorovi i veze
+            jasno vide u konzoli nakon svakog ispisa.  <br /> Način prikaza se ostavlja studentima.
         </PStyled>
         <Napomena>Napomena: Ovaj zadatak se šalje mejlom.</Napomena>
         <Napomena>Napomena: 3 poena nosi ceo zadatak. 2 poena se dobija za jednu implementiranu operaciju(brisanje/dodavanje).
